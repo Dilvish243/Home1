eaf4764b11fb42d21d1158c492337c1715eba163
Dilvish243 <132493923+Dilvish243@users.noreply.github.com>
2023-09-14 23:02:05 +0500

    14.09

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    public static bool isGameOver;
+    public GameObject gameOverScreen;
+
+    private void Awake()
+    {
+        isGameOver = false;
+    }
+
+
+    void Update()
+    {
+        if (isGameOver)
+        {
+            gameOverScreen.SetActive(true);
+        }
+    }
+
+    public void ReplayLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+}
